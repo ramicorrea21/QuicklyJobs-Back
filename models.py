2091a1c1ff135d76a69512cc4c7815c3104a91f1
ramicorrea21 <ramicorrea021@gmail.com>
2024-01-31 11:39:25 -0300

    population

--- models.py
+++ models.py
@@ -1,21 +1,91 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_handle = db.Column(db.String(50), nullable=False, unique=True)
-    user_email = db.Column(db.String(50), nullable=False)
-    user_name = db.Column(db.String(50), nullable=False)
-    user_lastname = db.Column(db.String(50), nullable=False)
+    user_email = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(50), nullable=False)
+    profile = db.relationship('Profile', backref='Users')
+    services = db.relationship('Services', backref='Users')
+    requests = db.relationship('Requests', backref='Users')
 
     def serialize(self):
         return{
             "id" : self.id,
             "user_handle" : self.user_handle,
             "user_email" : self.user_email,
-            "user_name" : self.user_name,
-            "user_lastname" : self.user_lastname,
         }
-#terminar de desarrollar mis models
+
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    phone = db.Column(db.String(10), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    profession = db.Column(db.String(200), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+        
+    def serialize(self):
+        return{
+            "id" : self.id,
+            "user_id" : self.user_id,
+            "first_name" : self.first_name,
+            "last_name": self.last_name,
+            "description" : self.description,
+            "phone" : self.phone,
+            "location": self.location,
+            "address" : self.address,
+            "profession" : self.profession,
+            "category" : self.category
+        }
+
+class Services(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    is_remote = db.Column(db.Boolean, nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    price_range = db.Column(db.String(100), nullable=False)
+
+    def serliaze(self):
+        return{
+        "id" : self.id,
+        "user_id" : self.user_id,
+        "title": self.title,
+        "description": self.description,
+        "category": self.category,
+        "is_remote": self.is_remote,
+        "location": self.location,
+        "price_range": self.price_range
+        }
+    
+class Requests(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    is_remote = db.Column(db.Boolean, nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    price_range = db.Column(db.String(100), nullable=False)
+
+    def serliaze(self):
+        return{
+        "id" : self.id,
+        "user_id" : self.user_id,
+        "title": self.title,
+        "description": self.description,
+        "category": self.category,
+        "is_remote": self.is_remote,
+        "location": self.location,
+        "price_range": self.price_range
+        }
+    
--- routes.py
+++ routes.py
@@ -1,15 +1,111 @@
 from flask import jsonify, request
 from app import app 
-from models import db, Users  
+from models import db, Users, Profile, Services, Requests
+import json
+import os
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+user_path = os.path.join(os.path.dirname(__file__), "users.json")
+profiles_path = os.path.join(os.path.dirname(__file__), "profiles.json")
+services_path = os.path.join(os.path.dirname(__file__), "services.json")
+requests_path = os.path.join(os.path.dirname(__file__), "requests.json")
 
-@app.route('/users', methods=['GET'])
-def obtener_usuarios():
-    usuarios = Users.query.all()
-    return jsonify(list(map(lambda usr : usr.serialize(), usuarios)))
+@app.route("/user-population", methods=["GET"])
+def user_population():
+    with open(user_path, "r") as file:
+        data = json.load(file)
+        file.close
+        
+        for user in data:
+            user = Users(
+                user_handle=user['user_handle'],
+                user_email=user['user_email'],
+                password = user['password']
+            )
+            db.session.add(user)
+        try:
+            db.session.commit()
+            return jsonify({"message":"users populated"})
+        except Exception as error:
+            db.session.rollback()
+            return jsonify({"error":f"{error.args}"})
 
-#desarrollar api 
+@app.route("/profiles-population", methods=["GET"])
+def profiles_population():
+    with open(profiles_path, "r") as file:
+        data = json.load(file)
+        file.close
+
+    for profile in data:
+        profile = Profile(
+            user_id=profile['user_id'],
+            first_name = profile['first_name'],
+            last_name = profile['last_name'],
+            description = profile['description'],
+            phone = profile['phone'],
+            location = profile['location'],
+            address = profile['address'],
+            profession = profile['profession'],
+            category = profile['category']
+        )
+        db.session.add(profile)
+
+    try:
+        db.session.commit()
+        return jsonify({"message":"profiles populated"})
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({"error":f"{error.args}"})
+
+@app.route('/services-population', methods=["GET"])
+def services_population():
+    with open(services_path, "r") as file:
+        data = json.load(file)
+        file.close
+    
+    for service in data:
+        service = Services(
+            user_id = service['user_id'],
+            title = service['title'],
+            description = service['description'],
+            category = service['category'],
+            is_remote = service['is_remote'],
+            location = service['location'],
+            price_range = service['price_range']
+        )
+        db.session.add(service)
+    try:
+        db.session.commit()
+        return jsonify({"message":"services populated"})
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({"error":f"{error.args}"})
+    
+@app.route('/requests-population', methods=["GET"])
+def requests_population():
+    with open(requests_path, "r") as file:
+        data = json.load(file)
+        file.close
+    
+    for request in data:
+        request = Requests(
+            user_id = request['user_id'],
+            title = request['title'],
+            description = request['description'],
+            category = request['category'],
+            is_remote = request['is_remote'],
+            location = request['location'],
+            price_range = request['price_range']
+        )
+        db.session.add(request)
+    try:
+        db.session.commit()
+        return jsonify({"message":"requests populated"})
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({"error":f"{error.args}"})
+
+
+
+
+
 
