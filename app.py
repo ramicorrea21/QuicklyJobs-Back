bc16dbad86c423a59db677ae30b61aa6164e3af9
ramicorrea21 <ramicorrea021@gmail.com>
2024-03-26 22:54:22 -0300

    details

--- app.py
+++ app.py
@@ -4,21 +4,21 @@ from models import db
 from flask_migrate import Migrate
 from dotenv import load_dotenv 
 from flask_jwt_extended import JWTManager
-import os
 from flask_cors import CORS
-
+from datetime import timedelta
+import os
 
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI') 
-app.config['JWT_SECRET_KEY'] = 'your_secret_key' 
-jwt = JWTManager(app) 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')  
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=30)  
+jwt = JWTManager(app)
 CORS(app)
 
 db.init_app(app)
 migrate = Migrate(app, db)
-
 
 from routes import *
 
